6df58fe818e1fd9854b1b3ac4a676529de99285e
Shabab <mmshabab90@gmail.com>
2017-12-10 14:43:04 +0100

    Add project files.

---
+++ HelloApp/HelloApp/GreetPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class GreetPage : ContentPage
+    {
+        public GreetPage()
+        {
+            InitializeComponent();
+
+            sliderHello.Value = 0.5;
+                        
+        }
+
+        private void Slider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            lblHello.Text = String.Format("Value is{0:F3}", e.NewValue);
+        }
+    }
+}
